4281fe2c62bef3fc9841bb6ed0d20f7bbc93cc0e
Italo Pescador Varzone <ipv@vimansca.com.br>
2024-06-04 15:53:12 -0300

    feat: Ajusta path do projeto todo.

--- src/telas/home/script.js
+++ src/telas/home/script.js
@@ -36,7 +36,7 @@ function hidePlayerInfo() {
 }
 
 function goToPerfil() {
-    window.location.href = '../perfil/index.html'; 
+    window.location.href = '../telas/perfil/index.html'; 
 }
 
 function loadGame(game) {
@@ -71,28 +71,28 @@ function loadGame(game) {
 function loadCacaPalavras() {
     const gameContainer = document.getElementById('game-container');
     gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="cacapalavras/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+        <iframe id="game-iframe" src="../../jogos/cacapalavras/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
     `;
 }
 
 function loadHangame() {
     const gameContainer = document.getElementById('game-container');
     gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="forca/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+        <iframe id="game-iframe" src="../../jogos/forca/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
     `;
 }
 
 function loadEcopuzzle() {
     const gameContainer = document.getElementById('game-container');
     gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+        <iframe id="game-iframe" src="../../jogos/ecopuzzle/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
     `;
 }
 
 function loadQuizODS() {
     const gameContainer = document.getElementById('game-container');
     gameContainer.innerHTML = `
-        <iframe id="game-iframe" src="quiz/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
+        <iframe id="game-iframe" src="../../jogos/quiz/index.html" style="width: 100%; height: 100%; border: none;"></iframe>
     `;
 }
 
